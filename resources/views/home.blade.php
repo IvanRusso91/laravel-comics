b528b3d461b54364e3a5f0caa551c462124e4f47
IvanRusso91 <iv.russo91@gmail.com>
2022-06-23 21:12:01 +0200

    fixato due immagini

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,44 +8,44 @@
     </div>
 
     <div class="box-cards">
-      <div class="container box-c">
-        <div class="card">
-            @foreach ($comics as $comic)
-                <img src="{{$comic->image}}" alt="">
-                <h3>{{$comic->title}}</h3>
-            @endforeach
-       </div>
-      </div>
+        <div class="container box-c">
+            <div class="card">
+                @foreach ($comics as $comic)
+                    <img src="{{$comic->image}}" alt="{{$comic->type}}">
+                    <h3>{{$comic->title}}</h3>
+                @endforeach
+            </div>
+        </div>
 
-      <div class="btn-box">
-        <a class="btn" href="#">Load More</a>
-      </div>
+        <div class="btn-box">
+            <a class="btn" href="#">Load More</a>
+        </div>
 
     </div>
     <div class="box-choice">
-      <div class="container">
-        <ul>
-          <li>
-            <a href="#"><img  class="image-a" src="{{ asset('img/buy-comics-digital-comics.png')}}" alt="loghi-choice"><p>digital comics</p></a>
-          </li>
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="#"><img  class="image-a" src="{{ asset('img/buy-comics-digital-comics.png')}}" alt="loghi-choice"><p>digital comics</p></a>
+                </li>
 
-          <li>
-            <a href="#"><img  class="image-b" src="{{ asset('img/buy-comics-merchandise.png')}}" alt="loghi-choice"><p>dc merchandise</p></a>
-          </li>
+                <li>
+                    <a href="#"><img  class="image-b" src="{{ asset('img/buy-comics-merchandise.png')}}" alt="loghi-choice"><p>dc merchandise</p></a>
+                </li>
 
-          <li>
-            <a href="#"><img  class="image-c" src="{{ asset('img/buy-comics-subscriptions.png')}}" alt="loghi-choice"><p>subscription</p></a>
-          </li>
+                <li>
+                    <a href="#"><img  class="image-c" src="{{ asset('img/buy-comics-subscriptions.png')}}" alt="loghi-choice"><p>subscription</p></a>
+                </li>
 
-          <li>
-            <a href="#"><img  class="image-d"  src="{{ asset('img/buy-comics-shop-locator.png')}}" alt="loghi-choice"><p>comic shop locator</p></a>
-          </li>
+                <li>
+                    <a href="#"><img  class="image-d"  src="{{ asset('img/buy-comics-shop-locator.png')}}" alt="loghi-choice"><p>comic shop locator</p></a>
+                </li>
 
-          <li>
-            <a href="#"><img  class="image-e" src="{{ asset('img/buy-dc-power-visa.svg')}}" alt="loghi-choice"><p>dc power visa</p></a>
-          </li>
-        </ul>
-      </div>
+                <li>
+                    <a href="#"><img  class="image-e" src="{{ asset('img/buy-dc-power-visa.svg')}}" alt="loghi-choice"><p>dc power visa</p></a>
+                </li>
+            </ul>
+        </div>
 
     </div>
   </main>
